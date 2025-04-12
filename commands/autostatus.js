07c8456ecb6bd13f8422056d29fa111f09bd0e65
Cnamyrl <housseindamerji2@gmail.com>
2025-04-12 21:34:58 +0200

    Update autostatus.js

--- commands/autostatus.js
+++ commands/autostatus.js
@@ -8,7 +8,7 @@ const channelInfo = {
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
             newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterName: 'Cnamy-Bot-V1',
             serverMessageId: -1
         }
     }
