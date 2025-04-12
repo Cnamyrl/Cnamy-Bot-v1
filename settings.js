5edbd03ab752d1cc8da26ba60b8078bc09b92699
Cnamyrl <housseindamerji2@gmail.com>
2025-04-12 18:56:32 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Bot-V1',
   author: 'Bot',
-  botName: "Knight Bot",
+  botName: "Cnamy-Bot-V1",
   botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  ownerNumber: '4916011111118', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
